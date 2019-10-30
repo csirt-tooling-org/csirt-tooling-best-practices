2825e584bd1a434b44985164b2954fb6f6b992d3
Alexandre Dulaunoy <a@foo.be>
2019-10-30 16:42:31 +0100

    chg: [doc] typo fixed

--- gen.sh
+++ gen.sh
@@ -1,1 +1,1 @@
-pandoc -f markdown --toc --pdf-engine=xelatex --variable mainfont="Gentium" --variable sansfont="Liberation Sans" --variable monofont="Roboto" --template eisvogel best-practices.md -o best-practises.pdf
+pandoc -f markdown --toc --pdf-engine=xelatex --variable mainfont="Gentium" --variable sansfont="Liberation Sans" --variable monofont="Roboto" --template eisvogel best-practices.md -o best-practices.pdf
